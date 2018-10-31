1474638918a15ad03a6026b77f95d092c5903d3c
Roman Mazurets <mazurets.roma@gmail.com>
2018-10-31 18:23:33 +0200

    add update user action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,14 @@ class UsersController < ApplicationController
     end
   end
 
-  def update; end
+  def update
+    user = User.find(params[:id])
+    if user.update(user_params)
+      render json: { user: user }, status: :ok
+    else
+      render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
 
   private
 
